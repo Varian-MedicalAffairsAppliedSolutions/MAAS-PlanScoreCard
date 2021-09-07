b3c5a45338926be0bd545da250b093652e93c9a5
Jordan Lovis <jalovis@lakeheadu.ca>
2021-09-07 01:30:54 +0000

    - Removed the PluginView (User Control)

--- PlanScoreCard/Views/PluginView.xaml.cs
+++ PlanScoreCard/Views/PluginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PlanScoreCard.Views.Plugins
+namespace PlanScoreCard.Views
 {
     /// <summary>
     /// Interaction logic for PluginView.xaml
     /// </summary>
-    public partial class PluginView : UserControl
+    public partial class PluginView : MetroWindow
     {
         public PluginView()
         {
