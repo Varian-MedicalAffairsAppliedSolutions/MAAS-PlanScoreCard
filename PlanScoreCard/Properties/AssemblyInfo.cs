066c4147bb38bb24a166d1a9d197d60816de5821
Matthew C Schmidt <mattcschmidt@hotmail.com>
2024-12-06 23:09:50 +0000

    Updated Planscorecard API to a nuget package to assist developers in building the Scorecard Visualizer.

--- PlanScoreCard/Properties/AssemblyInfo.cs
+++ PlanScoreCard/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using VMS.TPS.Common.Model.API;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.3.0.1")]
+[assembly: AssemblyFileVersion("3.3.0.1")]
